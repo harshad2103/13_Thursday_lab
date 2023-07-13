c63905523ae025e7d396777c2279a45b98f4d849
harshad2103 <112926913+harshad2103@users.noreply.github.com>
2023-07-13 17:31:50 +0530

    Update Question_2.java

--- Question_2.java
+++ Question_2.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Question_2 {
     public static void main(String[] args) {
         System.out.println("Operator Options:");
-        System.out.println("1. Addition (+)");
-        System.out.println("2. Subtraction (-)");
-        System.out.println("3. Multiplication (*)");
-        System.out.println("4. Division (/)");
-        System.out.println("5. Modulo (%)");
+        System.out.println("1. Arithmetic Operators");
+        System.out.println("2. Relational Operators");
+        System.out.println("3. Logical Operators");
+        System.out.println("4. Bitwise Operators");
+        System.out.println("5. Unary Operators");
 
         try (Scanner input = new Scanner(System.in)) {
 			System.out.print("Enter your choice (1-5): ");
@@ -17,19 +17,44 @@ public class Question_2 {
 
 			switch (choice) {
 			    case 1:
-			        System.out.println("You selected Addition (+)");
+			        System.out.println("Arithmetic Operators:");
+			        System.out.println("- Addition (+)");
+			        System.out.println("- Subtraction (-)");
+			        System.out.println("- Multiplication (*)");
+			        System.out.println("- Division (/)");
+			        System.out.println("- Modulo (%)");
 			        break;
 			    case 2:
-			        System.out.println("You selected Subtraction (-)");
+			        System.out.println("Relational Operators:");
+			        System.out.println("- Equal to (==)");
+			        System.out.println("- Not equal to (!=)");
+			        System.out.println("- Greater than (>)");
+			        System.out.println("- Less than (<)");
+			        System.out.println("- Greater than or equal to (>=)");
+			        System.out.println("- Less than or equal to (<=)");
 			        break;
 			    case 3:
-			        System.out.println("You selected Multiplication (*)");
+			        System.out.println("Logical Operators:");
+			        System.out.println("- Logical AND (&&)");
+			        System.out.println("- Logical OR (||)");
+			        System.out.println("- Logical NOT (!)");
 			        break;
 			    case 4:
-			        System.out.println("You selected Division (/)");
+			        System.out.println("Bitwise Operators:");
+			        System.out.println("- Bitwise AND (&)");
+			        System.out.println("- Bitwise OR (|)");
+			        System.out.println("- Bitwise XOR (^)");
+			        System.out.println("- Bitwise NOT (~)");
+			        System.out.println("- Left shift (<<)");
+			        System.out.println("- Right shift (>>)");
 			        break;
 			    case 5:
-			        System.out.println("You selected Modulo (%)");
+			        System.out.println("Unary Operators:");
+			        System.out.println("- Increment (++)");
+			        System.out.println("- Decrement (--)");
+			        System.out.println("- Unary plus (+)");
+			        System.out.println("- Unary minus (-)");
+			        System.out.println("- Logical NOT (!)");
 			        break;
 			    default:
 			        System.out.println("Invalid choice!");
