461bc71893bef5d35e574a5e337e866584a4f9d8
harshad2103 <112926913+harshad2103@users.noreply.github.com>
2023-07-14 18:23:56 +0530

    Update Question_2.java

--- Question_2.java
+++ Question_2.java
@@ -1,65 +1,139 @@
+//Q2)WAP using switch case which will display the option of all operators
+
+
 package Harshad;
 
 import java.util.Scanner;
 
 public class Question_2 {
-    public static void main(String[] args) {
-        System.out.println("Operator Options:");
-        System.out.println("1. Arithmetic Operators");
-        System.out.println("2. Relational Operators");
-        System.out.println("3. Logical Operators");
-        System.out.println("4. Bitwise Operators");
-        System.out.println("5. Unary Operators");
 
-        try (Scanner input = new Scanner(System.in)) {
-			System.out.print("Enter your choice (1-5): ");
-			int choice = input.nextInt();
+	public static void main(String[] args) {
+//				initializing the variables
+				int choice, num1, num2;
+				try (Scanner sc = new Scanner(System.in)) {
+					System.out.println("1: Arithmetic operators \n2: Assingment operators \n3: Bitwise operator \n4: Logical  operator \n5: Relational Operator \n6: Ternary operator");
+					
+					
+					
 
-			switch (choice) {
-			    case 1:
-			        System.out.println("Arithmetic Operators:");
-			        System.out.println("- Addition (+)");
-			        System.out.println("- Subtraction (-)");
-			        System.out.println("- Multiplication (*)");
-			        System.out.println("- Division (/)");
-			        System.out.println("- Modulo (%)");
-			        break;
-			    case 2:
-			        System.out.println("Relational Operators:");
-			        System.out.println("- Equal to (==)");
-			        System.out.println("- Not equal to (!=)");
-			        System.out.println("- Greater than (>)");
-			        System.out.println("- Less than (<)");
-			        System.out.println("- Greater than or equal to (>=)");
-			        System.out.println("- Less than or equal to (<=)");
-			        break;
-			    case 3:
-			        System.out.println("Logical Operators:");
-			        System.out.println("- Logical AND (&&)");
-			        System.out.println("- Logical OR (||)");
-			        System.out.println("- Logical NOT (!)");
-			        break;
-			    case 4:
-			        System.out.println("Bitwise Operators:");
-			        System.out.println("- Bitwise AND (&)");
-			        System.out.println("- Bitwise OR (|)");
-			        System.out.println("- Bitwise XOR (^)");
-			        System.out.println("- Bitwise NOT (~)");
-			        System.out.println("- Left shift (<<)");
-			        System.out.println("- Right shift (>>)");
-			        break;
-			    case 5:
-			        System.out.println("Unary Operators:");
-			        System.out.println("- Increment (++)");
-			        System.out.println("- Decrement (--)");
-			        System.out.println("- Unary plus (+)");
-			        System.out.println("- Unary minus (-)");
-			        System.out.println("- Logical NOT (!)");
-			        break;
-			    default:
-			        System.out.println("Invalid choice!");
-			        break;
-			}
-		}
-    }
+
+//					ask user to which operation run
+						System.out.println("\nEnter the number which operator you want to Perform: ");
+						choice = sc.nextInt();
+					
+
+						switch(choice) 
+						{
+						
+//		                For arithmetic operator
+							case 1:{
+								int add , sub ,div,mul,mod;
+								System.out.println("Your choice is Arithmetic operators");
+								System.out.println("\nEnter any two number");
+								num1 = sc.nextInt();
+								num2 = sc.nextInt();
+								add = num1 + num2;
+								sub = num1 - num2;
+								div = num1 / num2;
+								mul = num1 * num2;
+								mod = num1 % num2;
+								System.out.println("The Addition of 2 number is: "+ add);
+								System.out.println("The Subtraction of 2 number is: "+ sub);
+								System.out.println("The Division of 2 numberis: "+ div);
+								System.out.println("The Multiplication of 2 number is: "+ mul);
+								System.out.println("The Module of 2 number is: "+ mod);
+								break;
+								}
+							
+							
+//			            Assignment operator
+							case 2:{
+								System.out.println("Your choice is Assingment operators");
+								System.out.println("\nEnter the single number");
+								num1 = sc.nextInt();
+								
+								
+								System.out.println("num1+=5 : "+(num1+=5));
+								System.out.println("num1-=5 : "+(num1-=5));
+								System.out.println("num1*=5 : "+(num1*=5));
+								System.out.println("num1/=5 : " +(num1/=5));
+								System.out.println("num1%=5 : " + (num1%=5));
+								System.out.println("num1=5: "+(num1=5));
+								
+								break;
+								}
+							
+							
+//			            for bitwise operator
+							case 3:{
+								 System.out.print("Enter the first integer: ");
+							        
+									num1 = sc.nextInt();
+
+							        System.out.print("Enter the second integer: ");
+							        num2 = sc.nextInt();
+
+							        // Bitwise AND
+							        int bitwiseAnd = num1 & num2;
+							        System.out.println("Bitwise AND: " + bitwiseAnd);
+
+							        // Bitwise OR
+							        int bitwiseOr = num1 | num2;
+							        System.out.println("Bitwise OR: " + bitwiseOr);
+
+							        // Bitwise XOR
+							        int bitwiseXor = num1 ^ num2;
+							        System.out.println("Bitwise XOR: " + bitwiseXor);
+								break;
+								}
+							
+//			            for Logical operator
+							case 4:{
+								System.out.println("Your choice is Logical operators");
+								System.out.println("\nEnter the any two number");
+								num1 = sc.nextInt();
+								num2 = sc.nextInt();
+								System.out.println("(num1 > num2) && (num1 < num2)" +((num1 > num2) && (num1 < num2)) );
+								System.out.println("(num1 > num2) || (num1 < num2)" +((num1 > num2) || (num1 < num2)) );
+								System.out.println("!(num1 > num2)"+(!(num1 > num2)) );
+								
+								break;
+								}
+							
+//			            for relational operator
+							case 5:{
+								System.out.println("Your choice is Relational operators");
+								System.out.println("\nEnter the any two number");
+								num1 = sc.nextInt();
+								num2 = sc.nextInt();
+								System.out.println("for check num1 > num2: "+ (num1 > num2));
+								System.out.println("for check num1 < num2: "+ (num1 < num2));
+								System.out.println("for check num1 = num2: "+ (num1 == num2));
+								System.out.println("for check num1 <= to num2: "+ (num1 <= num2));
+								System.out.println("for check num1 >= equal to num2: "+ (num1 >= num2));
+								System.out.println("for check num1 is not equal to num2: "+ (num1 != num2));
+								
+								break;
+								}
+							
+							
+//	                   for ternary operator
+							case 6:{
+								System.out.println("Your choice is Ternary operators");
+								System.out.println("\nEnter the any two number");
+								num1 = sc.nextInt();
+								num2 = sc.nextInt();
+								
+								String res=(num1 > num2) ? "Num 1 is greater than Num 2 ": "Num 2 is greater than Num 2";	
+								System.out.println(res);
+								
+								break;
+								}
+							
+							default:{
+								System.out.println("invalid inpuit");
+							}
+						}
+				}		
+				}
 }
