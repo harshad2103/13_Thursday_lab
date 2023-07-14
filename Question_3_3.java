c738715656007cab58d881ccff3181664df52ff8
harshad2103 <112926913+harshad2103@users.noreply.github.com>
2023-07-14 18:21:11 +0530

    Update Question_3_3.java

--- Question_3_3.java
+++ Question_3_3.java
@@ -1,19 +1,1 @@
-package Harshad;
-
-import java.util.Scanner;
-
-public class Question_3_3 {
-    public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-			System.out.print("Enter a number: ");
-			int number = input.nextInt();
-
-			if (number % 2 == 0) {
-			    System.out.println(number + " is even.");
-			} else {
-			    System.out.println(number + " is odd.");
-			}
-		}
-    }
-}
 
